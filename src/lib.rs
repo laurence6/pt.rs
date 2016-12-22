d33ed8ad8bf69ec8fded6729b880ab6771196ce6
Laurence <liuxy6@gmail.com>
2016-12-22 14:36:48 -0500

    Add lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#[allow(non_snake_case)]
+pub mod vector;
+#[allow(non_snake_case)]
+pub mod matrix;
+//#[allow(non_snake_case)]
+//pub mod sphere;
+//#[allow(non_snake_case)]
+//pub mod ray;
+
+#[allow(non_snake_case)]
+mod common;
