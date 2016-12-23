f2f88f6c7cdc5ab937143a2c84ca34abaadadd97
Laurence <liuxy6@gmail.com>
2016-12-22 19:52:31 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
 #[allow(non_snake_case)]
+pub mod common;
+#[allow(non_snake_case)]
 pub mod vector;
 #[allow(non_snake_case)]
 pub mod matrix;
-//#[allow(non_snake_case)]
-//pub mod sphere;
-//#[allow(non_snake_case)]
-//pub mod ray;
 
 #[allow(non_snake_case)]
-mod common;
+pub mod ray;
+#[allow(non_snake_case)]
+pub mod camera;
+
+#[allow(non_snake_case)]
+pub mod material;
+
+#[allow(non_snake_case)]
+pub mod sphere;
