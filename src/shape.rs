f6631905850897645fbc85150b4ac25a73b25b37
Laurence <liuxy6@gmail.com>
2016-12-24 00:15:55 -0500

    Add shape.rs

---
+++ src/shape.rs
@@ -0,0 +1,16 @@
+use ray::Ray;
+use bbox::BBox;
+use material::Color;
+use hit::Hit;
+
+pub trait Shape {
+    fn BBox(&self) -> &BBox {
+        unimplemented!()
+    }
+    fn Intersect(&self, &Ray) -> Option<Hit> {
+        return None;
+    }
+    fn MateralAt(&self) -> Color {
+        unimplemented!()
+    }
+}
