a145632c9f52a3caf8abe26219ba1da72db8517a
Laurence <liuxy6@gmail.com>
2016-12-24 00:15:55 -0500

    Update matrix.rs

--- src/matrix.rs
+++ src/matrix.rs
@@ -34,3 +34,16 @@ pub fn Scale(v: &Vector) -> Matrix {
         m41: 0.0, m42: 0.0, m43: 0.0, m44: 1.0,
     };
 }
+
+pub fn Rotate(v: &Vector, a: Float) -> Matrix {
+    let v = v.Normalize();
+    let s = a.sin();
+    let c = a.cos();
+    let m = 1.0 - c;
+    return Matrix {
+        m11: m*v.X*v.X + c,     m12: m*v.X*v.Y + v.Z*s, m13: m*v.Z*v.X - v.Y*s, m14: 0.0,
+        m21: m*v.X*v.Y - v.Z*s, m22: m*v.Y*v.Y + c,     m23: m*v.Y*v.Z + v.X*s, m24: 0.0,
+        m31: m*v.Z*v.X + v.Y*s, m32: m*v.Y*v.Z - v.X*s, m33: m*v.Z*v.Z + c,     m34: 0.0,
+        m41: 0.0, m42: 0.0, m43: 0.0, m44: 1.0,
+    }
+}
