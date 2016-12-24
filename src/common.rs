78a70ae6bdcd9055631f41f48aabe53e80b640da
Laurence <liuxy6@gmail.com>
2016-12-24 00:15:55 -0500

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1,4 +1,14 @@
 pub type Float = f32;
 
-//pub const INF: Float = 2e9;
+pub const INF: Float = 2e9;
 pub const EPS: Float = 1e-9;
+
+pub fn clamp(x: Float) -> Float {
+    if x < 0.0 {
+        return 0.0;
+    } else if x > 1.0 {
+        return 1.0;
+    } else {
+        return x;
+    }
+}
