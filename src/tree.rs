7206f4d5610f6140ca943307c3d3452d3a2480da
Laurence <liuxy6@gmail.com>
2016-12-24 19:31:41 -0500

    Update tree.rs

--- src/tree.rs
+++ src/tree.rs
@@ -6,16 +6,24 @@ use common::Float;
 // k-d tree
 pub struct Tree<'a> {
     pub Shapes: Vec<&'a Shape>,
+    pub ShapeIndices: Vec<usize>,
+    pub NNode: usize, // TODO: remove?
     pub Root: Node,
 }
 
 impl<'a> Tree<'a> {
     pub fn New(shapes: Vec<&'a Shape>) -> Tree {
         print!("Building k-d tree ({} shapes) ... ", shapes.len());
-        let node = Node::New();
+
+        let mut shapeIndices = Vec::<usize>::new();
+        let node = Node::NewLeaf(Vec::<usize>::new(), &mut shapeIndices);
+
         println!("Done");
+
         return Tree {
             Shapes: shapes,
+            ShapeIndices: shapeIndices,
+            NNode: 0,
             Root: node,
         };
     }
@@ -25,15 +33,11 @@ impl<'a> Tree<'a> {
     }
 }
 
-pub enum SplitAxis {
-    X,
-    Y,
-    Z,
-}
+pub enum SplitAxis { X, Y, Z }
 
 pub enum SplitOrShape {
     Split(SplitAxis, Float), // Splite Point
-    NShape(u32),             // Number of Shapes
+    Shape(u32),             // Number of Shapes
 }
 
 pub struct Node {
@@ -42,15 +46,38 @@ pub struct Node {
 }
 
 impl Node {
-    pub fn New() -> Node {
+    pub fn NewInterior(axis: SplitAxis, point: Float) -> Node {
         return Node {
-            SplitOrShape: SplitOrShape::NShape(0),
+            SplitOrShape: SplitOrShape::Split(axis, point),
             Index: 0,
         };
     }
 
-    pub fn InitLeaf(&self) {
+    pub fn NewLeaf(mut shapes: Vec<usize>, shapeIndices: &mut Vec<usize>) -> Node {
+        let (Shape, Index) = match shapes.len() {
+            0 => {
+                (SplitOrShape::Shape(0), 0)
+            },
+            1 => {
+                (SplitOrShape::Shape(1), shapes[0])
+            },
+            n => {
+                let i = shapeIndices.len();
+                shapeIndices.append(&mut shapes);
+                (SplitOrShape::Shape(n as u32), i)
+            },
+        };
+        return Node {
+            SplitOrShape: Shape,
+            Index: Index,
+        };
+    }
 
+    pub fn IsLeaf(&self) -> bool {
+        return match self.SplitOrShape {
+            SplitOrShape::Split(_, _) => false,
+            SplitOrShape::Shape(_) => true,
+        };
     }
 }
 
