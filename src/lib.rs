5ceae7683fb2e2b9e0011ed631e158cff856fa3f
Laurence <liuxy6@gmail.com>
2016-12-26 15:32:48 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,26 @@
-#[allow(non_snake_case)]
+#[allow(non_snake_case, non_camel_case_types)]
 pub mod common;
-#[allow(non_snake_case)]
+#[allow(non_snake_case, non_camel_case_types)]
 pub mod vector;
-#[allow(non_snake_case)]
+#[allow(non_snake_case, non_camel_case_types)]
 pub mod matrix;
-#[allow(non_snake_case)]
+#[allow(non_snake_case, non_camel_case_types)]
 pub mod bbox;
-#[allow(non_snake_case)]
+#[allow(non_snake_case, non_camel_case_types)]
 pub mod ray;
-#[allow(non_snake_case)]
+#[allow(non_snake_case, non_camel_case_types)]
 pub mod hit;
-#[allow(non_snake_case)]
+#[allow(non_snake_case, non_camel_case_types)]
 pub mod camera;
 
-#[allow(non_snake_case)]
+#[allow(non_snake_case, non_camel_case_types)]
 pub mod shape;
-#[allow(non_snake_case)]
+#[allow(non_snake_case, non_camel_case_types)]
 pub mod tree;
 
-#[allow(non_snake_case)]
+#[allow(non_snake_case, non_camel_case_types)]
 pub mod sphere;
 
-#[allow(non_snake_case)]
+#[allow(non_snake_case, non_camel_case_types)]
 pub mod material;
 
