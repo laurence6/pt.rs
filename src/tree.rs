bd626d78cd09cd87318b83c52e07d9b38584e606
Laurence <liuxy6@gmail.com>
2016-12-26 15:32:48 -0500

    Update tree.rs

--- src/tree.rs
+++ src/tree.rs
@@ -7,7 +7,6 @@ use common::FLOAT_MIN_POS;
 use hit::Hit;
 use ray::Ray;
 use shape::Shape;
-use vector::Vector;
 
 // k-d tree
 pub struct Tree<'a> {
@@ -23,35 +22,30 @@ impl<'a> Tree<'a> {
     pub fn New(shapes: Vec<&'a Shape>, maxDepth: u8) -> Tree {
         print!("Building k-d tree ({} shapes) ... ", shapes.len());
 
-        let maxDepth = if maxDepth > 0 { maxDepth }
-                       else { (8.0 + 1.3 * (shapes.len() as f32).log(2.0)).round() as u8 };
-
         // Compute BBox
-        let mut bbox = BBox::New(Vector::ZeroVector(), Vector::ZeroVector() );
-        let mut bboxs = Vec::<BBox>::new();
-        for shape in &shapes {
-            let b = shape.BBox();
-            bbox = bbox.Union(&b);
-            bboxs.push(b);
-        }
-
-        let mut shapeNums = vec![0; shapes.len()];
-
-        // temp
-        let mut shapeIndices = Vec::<usize>::new();
-
+        let bbox = BBox::BBoxOfShapes(&shapes);
         let mut tree = Tree {
             Shapes: shapes,
-            ShapeIndices: shapeIndices,
+            ShapeIndices: Vec::<usize>::new(),
             NNode: 0,
             Nodes: Vec::<Node>::new(),
 
             bBox: bbox,
         };
 
+        let mut shapes = vec![0; tree.Shapes.len()];
+        for i in 0..shapes.len() {
+            shapes[i] = i;
+        }
         let nodeBBox = tree.bBox;
+        let maxDepth = if maxDepth > 0 {
+            maxDepth
+        } else {
+            (8.0 + 1.3 * (shapes.len() as f32).log(2.0)).round() as u8
+        };
 
-        let tree = buildTree(tree, shapeNums, &nodeBBox, maxDepth);
+
+        let tree = buildTree(tree, shapes, &nodeBBox, 0, maxDepth);
 
         return tree;
     }
@@ -61,10 +55,10 @@ impl<'a> Tree<'a> {
     }
 }
 
-const maxShapesInNode: usize = 8;
-const isectCost: Float = 80.0; // TODO
-const travCost: Float = 1.0; // TODO
-const emptyBonus: Float = 0.5;
+const MAX_SHAPES_IN_NODE: usize = 8;
+const ISECT_COST: Float = 80.0; // TODO
+const TRAV_COST: Float = 1.0; // TODO
+const EMPTY_BONUS: Float = 0.5;
 
 #[derive(PartialEq)]
 enum bEdgeType {
@@ -88,28 +82,31 @@ impl bEdge {
 // Recursive construction
 //   Decide if the node should be an interior node or leaf
 //   Update the data structures appropriately
-fn buildTree<'a>(mut tree: Tree<'a>,
-             mut shapes: Vec<usize>,
-             nodeBBox: &BBox,
-             depth: u8) -> Tree<'a> {
-    if shapes.len() <= maxShapesInNode || depth == 0 {
+fn buildTree<'a>(
+    mut tree: Tree<'a>,
+    mut shapes: Vec<usize>,
+    nodeBBox: &BBox,
+    mut badRefines: u8,
+    depth: u8) -> Tree<'a> {
+
+    if shapes.len() <= MAX_SHAPES_IN_NODE || depth == 0 {
         tree.Nodes.push(Node::NewLeaf(&mut shapes, &mut tree.ShapeIndices));
         return tree;
     }
 
+    let d = nodeBBox.Diagonal();
+    let totSA = nodeBBox.SurfaceArea();
+    let invTotSA = 1.0 / totSA;
+
     let mut bestAxis: Option<Axis> = None;
     let mut bestIndex = 0;
     let mut bestCost = FLOAT_MAX;
-    let mut oldCost = isectCost * shapes.len() as Float;
-    let totSA = nodeBBox.SurfaceArea();
-    let invTotSA = 1.0 / totSA;
-    let d = nodeBBox.Diagonal();
+    let mut oldCost = ISECT_COST * shapes.len() as Float;
 
     let mut edges = [Vec::<bEdge>::new(), Vec::<bEdge>::new(), Vec::<bEdge>::new()];
     let mut axis = nodeBBox.MaximumExtent();
 
-
-// RETRY
+    // try different axes
     for _ in 0..2 {
         for i in 0..shapes.len() {
             let s = tree.Shapes[i];
@@ -141,11 +138,19 @@ fn buildTree<'a>(mut tree: Tree<'a>,
 
             let t = edges[axis as usize][i].t;
             if nodeBBox.Min[axis] < t && t < nodeBBox.Max[axis] {
-                let (axis1, axis2) = axis.OtherAxes();
-                let pBelow = 2.0 * (d[axis1] * d[axis2] + (t - nodeBBox.Min[axis]) * (d[axis1] + d[axis2])) * invTotSA;
-                let pAbove = 2.0 * (d[axis1] * d[axis2] + (nodeBBox.Min[axis] - t) * (d[axis1] + d[axis2])) * invTotSA;
-                let bonus = if pBelow < FLOAT_MIN_POS || pAbove < FLOAT_MIN_POS { emptyBonus } else { 0.0 };
-                let cost = travCost + isectCost * (1.0 - bonus) * (pBelow * nBelow as Float + pAbove * nAbove as Float);
+                let (pBelow, pAbove) = {
+                    let (axis1, axis2) = axis.OtherAxes();
+                    (
+                        2.0 * (d[axis1] * d[axis2] + (t - nodeBBox.Min[axis]) * (d[axis1] + d[axis2])) * invTotSA,
+                        2.0 * (d[axis1] * d[axis2] + (nodeBBox.Min[axis] - t) * (d[axis1] + d[axis2])) * invTotSA,
+                    )
+                };
+                let bonus = if pBelow < FLOAT_MIN_POS || pAbove < FLOAT_MIN_POS {
+                    EMPTY_BONUS
+                } else {
+                    0.0
+                };
+                let cost = TRAV_COST + ISECT_COST * (1.0 - bonus) * (pBelow * nBelow as Float + pAbove * nAbove as Float);
                 if cost < bestCost {
                     bestCost = cost;
                     bestAxis = Some(axis);
@@ -166,8 +171,21 @@ fn buildTree<'a>(mut tree: Tree<'a>,
         }
     }
 
+    if bestCost > oldCost {
+        badRefines += 1;
+    }
 
-    return tree;
+    if (bestCost > 4.0 * oldCost && shapes.len() < 16) || bestAxis.is_none() || badRefines == 3 {
+        tree.Nodes.push(Node::NewLeaf(&mut shapes, &mut tree.ShapeIndices));
+        return tree;
+    }
+
+    {
+        let n0 = 0;
+        let n1 = 1;
+    }
+
+    return buildTree(tree, shapes, nodeBBox, badRefines, depth-1);
 }
 
 
