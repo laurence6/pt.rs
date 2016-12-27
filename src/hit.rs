71e82f29cb3b44aaabc9c1cafa5c059af11db68e
Laurence <liuxy6@gmail.com>
2016-12-26 23:32:23 -0500

    Update hit.rs

--- src/hit.rs
+++ src/hit.rs
@@ -1,13 +1,23 @@
 use common::Float;
 use shape::Shape;
 
-pub struct Hit<'a> {
-    pub Shape: &'a (Shape + 'a),
+//pub struct Hit<'a> {
+//    pub Shape: &'a (Shape + 'a),
+//    pub T: Float,
+// }
+//
+//impl<'a> Hit<'a> {
+//    pub fn New(shape: &'a Shape, t: Float) -> Hit {
+//        return Hit { Shape: shape, T: t };
+//    }
+// }
+
+pub struct Hit {
     pub T: Float,
 }
 
-impl<'a> Hit<'a> {
-    pub fn New(shape: &'a Shape, t: Float) -> Hit {
-        return Hit { Shape: shape, T: t };
+impl Hit {
+    pub fn New(t: Float) -> Hit {
+        return Hit { T: t };
     }
 }
