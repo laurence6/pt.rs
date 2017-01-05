9eb915b128c7c8b1b197d04f4f5b255435643dde
Laurence <liuxy6@gmail.com>
2017-01-05 18:20:07 -0500

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -11,50 +11,13 @@ macro_rules! DefFloat {
     );
 }
 
+// Use f32 by default
+#[cfg(not(feature = "f64"))]
 DefFloat!(f32);
+#[cfg(feature = "f64")]
+DefFloat!(f64);
 
-// Axis
-use std::convert::Into;
-
-#[derive(Clone, Copy, Debug)]
-pub enum Axis {
-    X,
-    Y,
-    Z,
-}
-
-impl Axis {
-    pub fn NextAxis(&self) -> Axis {
-        match *self {
-            Axis::X => Axis::Y,
-            Axis::Y => Axis::Z,
-            Axis::Z => Axis::X,
-        }
-    }
-    pub fn OtherAxes(&self) -> (Axis, Axis) {
-        match *self {
-            Axis::X => (Axis::Y, Axis::Z),
-            Axis::Y => (Axis::X, Axis::Z),
-            Axis::Z => (Axis::X, Axis::Y),
-        }
-    }
-}
-
-impl Into<usize> for Axis {
-    fn into(self) -> usize {
-        match self {
-            Axis::X => 0,
-            Axis::Y => 1,
-            Axis::Z => 2,
-        }
-    }
-}
-
-// funcs
-pub fn gamma(x: Float) -> Float {
-    return (x * EPSILON_HALF) / (1.0 - x * EPSILON_HALF);
-}
-
+// Funcs
 pub fn clamp(x: Float) -> Float {
     if x < 0.0 {
         return 0.0;
