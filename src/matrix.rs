a3092ec50b40eb700a38da72afabea38ea635da9
Laurence <liuxy6@gmail.com>
2017-01-05 14:29:59 -0500

    Update matrix.rs

--- src/matrix.rs
+++ src/matrix.rs
@@ -8,14 +8,12 @@ pub struct Matrix {
     pub m41: Float, pub m42: Float, pub m43: Float, pub m44: Float,
 }
 
-pub fn Identity() -> Matrix {
-    return Matrix {
-        m11: 1.0, m12: 0.0, m13: 0.0, m14: 0.0,
-        m21: 0.0, m22: 1.0, m23: 0.0, m24: 0.0,
-        m31: 0.0, m32: 0.0, m33: 1.0, m34: 0.0,
-        m41: 0.0, m42: 0.0, m43: 0.0, m44: 1.0,
-    };
-}
+pub const IDENTITY_MATRIX: Matrix = Matrix {
+    m11: 1.0, m12: 0.0, m13: 0.0, m14: 0.0,
+    m21: 0.0, m22: 1.0, m23: 0.0, m24: 0.0,
+    m31: 0.0, m32: 0.0, m33: 1.0, m34: 0.0,
+    m41: 0.0, m42: 0.0, m43: 0.0, m44: 1.0,
+};
 
 pub fn Translate(v: &Vector) -> Matrix {
     return Matrix {
