6b6c98b006fc11bbe63a82c5f965cb645aed8ec4
Laurence <liuxy6@gmail.com>
2017-01-06 14:33:07 -0500

    Update matrix.rs

--- src/matrix.rs
+++ src/matrix.rs
@@ -23,25 +23,25 @@ pub fn Translate(v: &Vector) -> Matrix {
         m41: 0.0, m42: 0.0, m43: 0.0, m44: 1.0,
     };
 }
-
-pub fn Scale(v: &Vector) -> Matrix {
-    return Matrix {
-        m11: v.X, m12: 0.0, m13: 0.0, m14: 0.0,
-        m21: 0.0, m22: v.Y, m23: 0.0, m24: 0.0,
-        m31: 0.0, m32: 0.0, m33: v.Z, m34: 0.0,
-        m41: 0.0, m42: 0.0, m43: 0.0, m44: 1.0,
-    };
-}
-
-pub fn Rotate(v: &Vector, a: Float) -> Matrix {
-    let v = v.Normalize();
-    let s = a.sin();
-    let c = a.cos();
-    let m = 1.0 - c;
-    return Matrix {
-        m11: m*v.X*v.X + c,     m12: m*v.X*v.Y + v.Z*s, m13: m*v.Z*v.X - v.Y*s, m14: 0.0,
-        m21: m*v.X*v.Y - v.Z*s, m22: m*v.Y*v.Y + c,     m23: m*v.Y*v.Z + v.X*s, m24: 0.0,
-        m31: m*v.Z*v.X + v.Y*s, m32: m*v.Y*v.Z - v.X*s, m33: m*v.Z*v.Z + c,     m34: 0.0,
-        m41: 0.0, m42: 0.0, m43: 0.0, m44: 1.0,
-    }
-}
+//
+//pub fn Scale(v: &Vector) -> Matrix {
+//    return Matrix {
+//        m11: v.X, m12: 0.0, m13: 0.0, m14: 0.0,
+//        m21: 0.0, m22: v.Y, m23: 0.0, m24: 0.0,
+//        m31: 0.0, m32: 0.0, m33: v.Z, m34: 0.0,
+//        m41: 0.0, m42: 0.0, m43: 0.0, m44: 1.0,
+//    };
+// }
+//
+//pub fn Rotate(v: &Vector, a: Float) -> Matrix {
+//    let v = v.Normalize();
+//    let s = a.sin();
+//    let c = a.cos();
+//    let m = 1.0 - c;
+//    return Matrix {
+//        m11: m*v.X*v.X + c,     m12: m*v.X*v.Y + v.Z*s, m13: m*v.Z*v.X - v.Y*s, m14: 0.0,
+//        m21: m*v.X*v.Y - v.Z*s, m22: m*v.Y*v.Y + c,     m23: m*v.Y*v.Z + v.X*s, m24: 0.0,
+//        m31: m*v.Z*v.X + v.Y*s, m32: m*v.Y*v.Z - v.X*s, m33: m*v.Z*v.Z + c,     m34: 0.0,
+//        m41: 0.0, m42: 0.0, m43: 0.0, m44: 1.0,
+//    }
+// }
