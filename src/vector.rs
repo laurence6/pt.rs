cb237cf5e961ddd64052159a2c4fe8b08f9b9a7e
Laurence <liuxy6@gmail.com>
2017-01-08 01:21:18 -0500

    Update vector.rs
      - Use operator overloading

--- src/vector.rs
+++ src/vector.rs
@@ -23,13 +23,6 @@ impl Vector {
         return Vector { X: x, Y: y, Z: z };
     }
 
-    //pub fn RandomUnitVector() -> Vector {
-    //    let x = rand::random::<Float>();
-    //    let y = rand::random::<Float>();
-    //    let z = rand::random::<Float>();
-    //    return V(x, y, z).Normalize();
-    // }
-
     pub fn Length(&self) -> Float {
         return (self.X * self.X + self.Y * self.Y + self.Z * self.Z).sqrt();
     }
@@ -37,10 +30,6 @@ impl Vector {
     pub fn Normalize(&self) -> Vector {
         let l = self.Length();
         return V(self.X / l, self.Y / l, self.Z / l);
-    }
-
-    pub fn Neg(&self) -> Vector {
-        return V(-self.X, -self.Y, -self.Z);
     }
 
     pub fn Inv(&self) -> Vector {
@@ -60,45 +49,6 @@ impl Vector {
         let y = self.Z * v.X - self.X * v.Z;
         let z = self.X * v.Y - self.Y * v.X;
         return V(x, y, z);
-    }
-
-    pub fn Add(&self, v: &Vector) -> Vector {
-        return V(self.X + v.X, self.Y + v.Y, self.Z + v.Z);
-    }
-
-    pub fn Sub(&self, v: &Vector) -> Vector {
-        return V(self.X - v.X, self.Y - v.Y, self.Z - v.Z);
-    }
-
-    //pub fn Mul(&self, v: &Vector) -> Vector {
-    //    return V(self.X * v.X, self.Y * v.Y, self.Z * v.Z);
-    // }
-
-    //pub fn Div(&self, v: &Vector) -> Vector {
-    //    return V(self.X / v.X, self.Y / v.Y, self.Z / v.Z);
-    // }
-
-    //pub fn Rem(&self, v: &Vector) -> Vector {
-    //    let x = self.X - v.X * (self.X / v.X).floor();
-    //    let y = self.Y - v.Y * (self.Y / v.Y).floor();
-    //    let z = self.Z - v.Z * (self.Z / v.Z).floor();
-    //    return V(x, y, z);
-    // }
-
-    pub fn AddScalar(&self, s: Float) -> Vector {
-        return V(self.X + s, self.Y + s, self.Z + s);
-    }
-
-    pub fn SubScalar(&self, s: Float) -> Vector {
-        return V(self.X - s, self.Y - s, self.Z - s);
-    }
-
-    pub fn MulScalar(&self, s: Float) -> Vector {
-        return V(self.X * s, self.Y * s, self.Z * s);
-    }
-
-    pub fn DivScalar(&self, s: Float) -> Vector {
-        return V(self.X / s, self.Y / s, self.Z / s);
     }
 
     pub fn Min(&self, v: &Vector) -> Vector {
@@ -127,7 +77,7 @@ impl Vector {
     }
 
     pub fn Reflect(&self, v: &Vector) -> Vector {
-        return v.Sub(&self.MulScalar(2.0 * self.Dot(v)));
+        return *v - (*self * (2.0 * self.Dot(v)));
     }
 
     pub fn Refract(&self, v: &Vector, n1: Float, n2: Float) -> Vector {
@@ -138,7 +88,7 @@ impl Vector {
             return V(0.0, 0.0, 0.0);
         }
         let cosT = (1.0 - sinT2).sqrt();
-        return v.MulScalar(nr).Add(&self.MulScalar(nr * cosI - cosT));
+        return (*v * nr) + (*self * (nr * cosI - cosT));
     }
 
     pub fn Reflectance(&self, v: &Vector, n1: Float, n2: Float) -> Float {
@@ -158,70 +108,49 @@ impl Vector {
 impl ops::Neg for Vector {
     type Output = Vector;
     fn neg(self) -> Vector {
-        self.Neg()
+        return V(-self.X, -self.Y, -self.Z);
     }
 }
 
 impl ops::Add<Vector> for Vector {
     type Output = Vector;
     fn add(self, v: Vector) -> Vector {
-        self.Add(&v)
+        return V(self.X + v.X, self.Y + v.Y, self.Z + v.Z);
     }
 }
 
 impl ops::Sub<Vector> for Vector {
     type Output = Vector;
     fn sub(self, v: Vector) -> Vector {
-        self.Sub(&v)
+        return V(self.X - v.X, self.Y - v.Y, self.Z - v.Z);
     }
 }
-
-//impl ops::Mul<Vector> for Vector {
-//    type Output = Vector;
-//    fn mul(self, v: Vector) -> Vector {
-//        self.Mul(&v)
-//    }
-// }
-//
-//impl ops::Div<Vector> for Vector {
-//    type Output = Vector;
-//    fn div(self, v: Vector) -> Vector {
-//        self.Div(&v)
-//    }
-// }
-//
-//impl ops::Rem<Vector> for Vector {
-//    type Output = Vector;
-//    fn rem(self, v: Vector) -> Vector {
-//        self.Rem(&v)
-//    }
-// }
 
 impl ops::Add<Float> for Vector {
     type Output = Vector;
     fn add(self, s: Float) -> Vector {
-        self.AddScalar(s)
+        return V(self.X + s, self.Y + s, self.Z + s);
     }
 }
 
 impl ops::Sub<Float> for Vector {
     type Output = Vector;
     fn sub(self, s: Float) -> Vector {
-        self.SubScalar(s)
+        return V(self.X - s, self.Y - s, self.Z - s);
     }
 }
 
 impl ops::Mul<Float> for Vector {
     type Output = Vector;
     fn mul(self, s: Float) -> Vector {
-        self.MulScalar(s)
+        return V(self.X * s, self.Y * s, self.Z * s);
     }
 }
 
 impl ops::Div<Float> for Vector {
     type Output = Vector;
     fn div(self, s: Float) -> Vector {
-        self.DivScalar(s)
+        return V(self.X / s, self.Y / s, self.Z / s);
     }
 }
 
