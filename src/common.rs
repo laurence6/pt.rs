eafac70bbe0ce94336a10c1963c826f9c3b34271
Laurence <liuxy6@gmail.com>
2017-01-08 23:27:23 -0500

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1,12 +1,12 @@
 // Type Float
 macro_rules! DefFloat {
-    ($v: ident) => (
-        use std::$v;
-        pub type Float = $v;
-        pub const FLOAT_MAX: Float     = $v::MAX;
-        pub const FLOAT_MIN_POS: Float = $v::MIN_POSITIVE;
-        pub const EPSILON: Float       = $v::EPSILON * 0.5;
-        pub const INFINITY: Float      = $v::INFINITY;
+    ($f: ident) => (
+        use std::$f;
+        pub type Float = $f;
+        pub const FLOAT_MAX: Float     = $f::MAX;
+        pub const FLOAT_MIN_POS: Float = $f::MIN_POSITIVE;
+        pub const EPSILON: Float       = $f::EPSILON * 0.5;
+        pub const INFINITY: Float      = $f::INFINITY;
 
         // Max number less than 1
         pub const ONE_MINUS_EPSILON: Float = 1.0 - EPSILON;
@@ -20,11 +20,22 @@ DefFloat!(f32);
 DefFloat!(f64);
 
 // Funcs
-pub fn clamp(x: Float) -> Float {
-    if x < 0.0 {
-        return 0.0;
-    } else if x > 1.0 {
-        return 1.0;
+//pub fn Clamp<T: PartialOrd>(x: T, low: T, high: T) -> T {
+//    //debug_assert!(low <= high);
+//    if x < low {
+//        return low;
+//    } else if x > high {
+//        return high;
+//    } else {
+//        return x;
+//    }
+// }
+pub fn Clamp(x: Float, low: Float, high: Float) -> Float {
+    debug_assert!(low <= high);
+    if x < low {
+        return low;
+    } else if x > high {
+        return high;
     } else {
         return x;
     }
