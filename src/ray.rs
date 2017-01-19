e989137b1abfcdc2be22b7adfd28a9854d6d07fd
Laurence <liuxy6@gmail.com>
2017-01-18 21:29:11 -0500

    Update ray.rs

--- src/ray.rs
+++ src/ray.rs
@@ -23,17 +23,17 @@ impl Ray {
         return self.Origin + self.Direction * t;
     }
 
-    pub fn Reflect(&self, i: &Ray) -> Ray {
-        return Ray::New(self.Origin, self.Direction.Reflect(&i.Direction));
-    }
+    //pub fn Reflect(&self, i: &Ray) -> Ray {
+    //    return Ray::New(self.Origin, self.Direction.Reflect(&i.Direction));
+    // }
 
-    pub fn Refract(&self, i: &Ray, n1: Float, n2: Float) -> Ray {
-        return Ray::New(self.Origin, self.Direction.Refract(&i.Direction, n1, n2));
-    }
+    //pub fn Refract(&self, i: &Ray, n1: Float, n2: Float) -> Ray {
+    //    return Ray::New(self.Origin, self.Direction.Refract(&i.Direction, n1, n2));
+    // }
 
-    pub fn Reflectance(&self, i: &Ray, n1: Float, n2: Float) -> Float {
-        return self.Direction.Reflectance(&i.Direction, n1, n2);
-    }
+    //pub fn Reflectance(&self, i: &Ray, n1: Float, n2: Float) -> Float {
+    //    return self.Direction.Reflectance(&i.Direction, n1, n2);
+    // }
 
     pub fn WeightedBounce() -> Ray {
         unimplemented!()
