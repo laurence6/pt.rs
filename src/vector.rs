73cfcb2488852475125ebc6dd934b8305bb917b7
Laurence <liuxy6@gmail.com>
2017-01-18 21:29:11 -0500

    Update vector.rs

--- src/vector.rs
+++ src/vector.rs
@@ -3,61 +3,62 @@ use std::ops;
 use common::Float;
 use axis::Axis;
 
+pub type Vector = Vector3f;
+pub const ZERO_VECTOR: Vector3f = Vector3f { X: 0.0, Y: 0.0, Z: 0.0 };
+
 #[derive(Clone, Copy, Debug)]
-pub struct Vector {
+pub struct Vector3f {
     pub X: Float,
     pub Y: Float,
     pub Z: Float,
 }
 
-pub const ZERO_VECTOR: Vector = Vector { X: 0.0, Y: 0.0, Z: 0.0 };
-
-fn V(x: Float, y: Float, z: Float) -> Vector {
-    return Vector::New(x, y, z);
+fn V(x: Float, y: Float, z: Float) -> Vector3f {
+    return Vector3f::New(x, y, z);
 }
 
-impl Vector {
-    pub fn New(x: Float, y: Float, z: Float) -> Vector {
-        return Vector { X: x, Y: y, Z: z };
+impl Vector3f {
+    pub fn New(x: Float, y: Float, z: Float) -> Vector3f {
+        return Vector3f { X: x, Y: y, Z: z };
     }
 
     pub fn Length(&self) -> Float {
         return (self.X * self.X + self.Y * self.Y + self.Z * self.Z).sqrt();
     }
 
-    pub fn Normalize(&self) -> Vector {
+    pub fn Normalize(&self) -> Vector3f {
         let l = self.Length();
         return V(self.X / l, self.Y / l, self.Z / l);
     }
 
-    pub fn Inv(&self) -> Vector {
+    pub fn Inv(&self) -> Vector3f {
         return V(1.0 / self.X, 1.0 / self.Y, 1.0 / self.Z);
     }
 
-    pub fn Abs(&self) -> Vector {
+    pub fn Abs(&self) -> Vector3f {
         return V(self.X.abs(), self.Y.abs(), self.Z.abs());
     }
 
-    pub fn Dot(&self, v: &Vector) -> Float {
+    pub fn Dot(&self, v: &Vector3f) -> Float {
         return self.X * v.X + self.Y * v.Y + self.Z * v.Z;
     }
 
-    pub fn Cross(&self, v: &Vector) -> Vector {
+    pub fn Cross(&self, v: &Vector3f) -> Vector3f {
         let x = self.Y * v.Z - self.Z * v.Y;
         let y = self.Z * v.X - self.X * v.Z;
         let z = self.X * v.Y - self.Y * v.X;
         return V(x, y, z);
     }
 
-    pub fn Min(&self, v: &Vector) -> Vector {
+    pub fn Min(&self, v: &Vector3f) -> Vector3f {
         return V(self.X.min(v.X), self.Y.min(v.Y), self.Z.min(v.Z));
     }
 
-    pub fn Max(&self, v: &Vector) -> Vector {
+    pub fn Max(&self, v: &Vector3f) -> Vector3f {
         return V(self.X.max(v.X), self.Y.max(v.Y), self.Z.max(v.Z));
     }
 
-    pub fn MinAxis(&self) -> Vector {
+    pub fn MinAxis(&self) -> Vector3f {
         let (x, y, z) = (self.X.abs(), self.Y.abs(), self.Z.abs());
         match (x <= y, y <= z) {
              (true,  true) => return V(1.0, 0.0, 0.0),
@@ -73,86 +74,58 @@ impl Vector {
     pub fn MaxComponent(&self) -> Float {
         return self.X.max(self.Y).max(self.Z);
     }
-
-    pub fn Reflect(&self, v: &Vector) -> Vector {
-        return *v - (*self * (2.0 * self.Dot(v)));
-    }
-
-    pub fn Refract(&self, v: &Vector, n1: Float, n2: Float) -> Vector {
-        let nr = n1 / n2;
-        let cosI = -self.Dot(v);
-        let sinT2 = nr * nr * (1.0 - cosI * cosI);
-        if sinT2 > 1.0 {
-            return V(0.0, 0.0, 0.0);
-        }
-        let cosT = (1.0 - sinT2).sqrt();
-        return (*v * nr) + (*self * (nr * cosI - cosT));
-    }
-
-    pub fn Reflectance(&self, v: &Vector, n1: Float, n2: Float) -> Float {
-        let nr = n1 / n2;
-        let cosI = -self.Dot(v);
-        let sinT2 = nr * nr * (1.0 - cosI * cosI);
-        if sinT2 > 1.0 {
-            return 1.0;
-        }
-        let cosT = (1.0 - sinT2).sqrt();
-        let rOrth = (n1 * cosI - n2 * cosT) / (n1 * cosI + n2 * cosT);
-        let rPar = (n2 * cosI - n1 * cosT) / (n2 * cosI + n1 * cosT);
-        return (rOrth * rOrth + rPar * rPar) / 2.0;
-    }
 }
 
-impl ops::Neg for Vector {
-    type Output = Vector;
-    fn neg(self) -> Vector {
+impl ops::Neg for Vector3f {
+    type Output = Vector3f;
+    fn neg(self) -> Vector3f {
         return V(-self.X, -self.Y, -self.Z);
     }
 }
 
-impl ops::Add<Vector> for Vector {
-    type Output = Vector;
-    fn add(self, v: Vector) -> Vector {
+impl ops::Add<Vector3f> for Vector3f {
+    type Output = Vector3f;
+    fn add(self, v: Vector3f) -> Vector3f {
         return V(self.X + v.X, self.Y + v.Y, self.Z + v.Z);
     }
 }
 
-impl ops::Sub<Vector> for Vector {
-    type Output = Vector;
-    fn sub(self, v: Vector) -> Vector {
+impl ops::Sub<Vector3f> for Vector3f {
+    type Output = Vector3f;
+    fn sub(self, v: Vector3f) -> Vector3f {
         return V(self.X - v.X, self.Y - v.Y, self.Z - v.Z);
     }
 }
 
-impl ops::Add<Float> for Vector {
-    type Output = Vector;
-    fn add(self, a: Float) -> Vector {
+impl ops::Add<Float> for Vector3f {
+    type Output = Vector3f;
+    fn add(self, a: Float) -> Vector3f {
         return V(self.X + a, self.Y + a, self.Z + a);
     }
 }
 
-impl ops::Sub<Float> for Vector {
-    type Output = Vector;
-    fn sub(self, a: Float) -> Vector {
+impl ops::Sub<Float> for Vector3f {
+    type Output = Vector3f;
+    fn sub(self, a: Float) -> Vector3f {
         return V(self.X - a, self.Y - a, self.Z - a);
     }
 }
 
-impl ops::Mul<Float> for Vector {
-    type Output = Vector;
-    fn mul(self, a: Float) -> Vector {
+impl ops::Mul<Float> for Vector3f {
+    type Output = Vector3f;
+    fn mul(self, a: Float) -> Vector3f {
         return V(self.X * a, self.Y * a, self.Z * a);
     }
 }
 
-impl ops::Div<Float> for Vector {
-    type Output = Vector;
-    fn div(self, a: Float) -> Vector {
+impl ops::Div<Float> for Vector3f {
+    type Output = Vector3f;
+    fn div(self, a: Float) -> Vector3f {
         return V(self.X / a, self.Y / a, self.Z / a);
     }
 }
 
-impl ops::Index<Axis> for Vector {
+impl ops::Index<Axis> for Vector3f {
     type Output = Float;
     fn index(&self, axis: Axis) -> &Float {
         match axis {
@@ -163,7 +136,7 @@ impl ops::Index<Axis> for Vector {
     }
 }
 
-impl ops::IndexMut<Axis> for Vector {
+impl ops::IndexMut<Axis> for Vector3f {
     fn index_mut<'a>(&'a mut self, axis: Axis) -> &'a mut Float {
         match axis {
             Axis::X => &mut self.X,
@@ -172,3 +145,22 @@ impl ops::IndexMut<Axis> for Vector {
         }
     }
 }
+
+macro_rules! def2 {
+    ($n: ident, $t: ident, $x: ident, $y: ident) => (
+        #[derive(Clone, Copy, Debug)]
+        pub struct $n {
+            pub $x: $t,
+            pub $y: $t,
+        }
+
+        impl $n {
+            pub fn New(x: $t, y: $t) -> $n {
+                return $n { $x: x, $y: y };
+            }
+        }
+    );
+}
+
+def2!(Point2i, i32, X, Y);
+def2!(Point2f, Float, X, Y);
