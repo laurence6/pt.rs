724a6e1d099c4009a1808ac017817fccd7a2adea
Laurence <liuxy6@gmail.com>
2017-02-01 15:34:17 -0500

    Remove return

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,8 @@ pub mod film;
 pub mod sampler;
 #[allow(non_snake_case, non_camel_case_types)]
 pub mod spectrum;
+#[allow(non_snake_case, non_camel_case_types)]
+pub mod reflection;
 
 #[allow(non_snake_case, non_camel_case_types)]
 pub mod shape;
