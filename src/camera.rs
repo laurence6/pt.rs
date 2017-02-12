7be6aeb2461f5a99341cf7fe4f7ba18d2c72b0a8
Laurence <liuxy6@gmail.com>
2017-02-12 14:31:58 -0500

    Update camera.rs

--- src/camera.rs
+++ src/camera.rs
@@ -1,15 +1,15 @@
-use vector::Point2f;
-use ray::Ray;
 use film::Film;
-
-pub struct Camera {
-    pub Film: Film,
-}
+use ray::Ray;
+use vector::Point2f;
 
 /// Records the position on Film that Camera should generate corresponding ray.
 pub struct CameraSample {
     pub pFilm: Point2f,
     pub pLens: Point2f,
+}
+
+pub struct Camera {
+    pub Film: Film,
 }
 
 impl Camera {
@@ -18,8 +18,17 @@ impl Camera {
             Film: film,
         };
     }
+}
 
+impl Camera {
+    /// Generate the world space ray corresponding to a sample position on the film plane.
     pub fn GenerateRay(sample: CameraSample) -> Ray {
         unimplemented!()
+    }
+
+    /// Generate the world space ray corresponding to a sample position on the film plane,
+    /// and compute the information about the image area.
+    pub fn GenerateRayDifferential() {
+
     }
 }
