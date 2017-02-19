e04787ea90d0533a2db24b1fc8913609eafdcc5a
Laurence <liuxy6@gmail.com>
2017-02-19 17:27:45 -0500

    add temp allow dead_code & unused_variables

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,39 @@
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod common;
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod axis;
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod vector;
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod matrix;
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod bbox;
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod ray;
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod hit;
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod camera;
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod film;
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod sampler;
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod spectrum;
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod reflection;
 
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod shape;
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod tree;
 
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod sphere;
 
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod material;
 
 // Sampler
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
 pub mod sampler_halton;
