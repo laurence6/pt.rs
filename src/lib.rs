dfc5166d2f2bda81031a74f622744a1a8284abd8
Laurence <liuxy6@gmail.com>
2017-03-15 22:15:00 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,21 @@
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod common;
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod axis;
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod vector;
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod matrix;
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod bbox;
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod ray;
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod hit;
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod camera;
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod film;
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod sampler;
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod spectrum;
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod reflection;
-
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod shape;
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod tree;
-
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod sphere;
-
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod material;
-
-// Sampler
-#[allow(non_snake_case, non_camel_case_types,   dead_code, unused_variables)]
-pub mod sampler_halton;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod axis;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod bbox;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod camera;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod common;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod film;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod hit;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod interaction;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod light;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod material;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod matrix;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod primitive;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod ray;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod reflection;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod sampler;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod sampler_halton;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod scene;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod shape;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod spectrum;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod sphere;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod tree;
+#[allow(non_snake_case, non_camel_case_types, dead_code, unused_variables)] mod vector;
