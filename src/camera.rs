270bd6e6a64c131a95a8c08dca70fb8a8d0a2b32
Laurence <liuxy6@gmail.com>
2017-05-20 20:54:03 +0800

    Fix screen_to_raster computation in PerspectiveCamera::new()

--- src/camera.rs
+++ src/camera.rs
@@ -18,9 +18,8 @@ pub trait Camera {
 pub struct PerspectiveCamera {
     camera_to_world: Transform,
 
-    camera_to_screen: Transform,
     raster_to_camera: Transform,
-
+    camera_to_screen: Transform,
     screen_to_raster: Transform,
     raster_to_screen: Transform,
 }
@@ -29,9 +28,21 @@ impl PerspectiveCamera {
     pub fn new(camera_to_world: Transform, screen_window: BBox2f, film_res: Point2u, fov: Float) -> PerspectiveCamera {
         let camera_to_screen = Transform::perspective(fov, 1.0e-2, 1000.0);
 
-        let screen_to_raster = Transform::scale(Vector3f::new(film_res.x as Float, film_res.y as Float, 1.0))
-                             * Transform::scale(Vector3f::new(1.0 / (screen_window.max.x - screen_window.min.y), 1.0 / (screen_window.max.y - screen_window.min.y), 1.0))
-                             * Transform::translate(Vector3f::new(-screen_window.min.x, -screen_window.max.y, 0.0));
+        let screen_to_raster = Transform::scale(Vector3f::new(
+                                    film_res.x as Float,
+                                    film_res.y as Float,
+                                    1.0,
+                               )) // 3. scale to raster space
+                             * Transform::scale(Vector3f::new(
+                                    1.0 / (screen_window.max.x - screen_window.min.x),
+                                    1.0 / (screen_window.min.y - screen_window.max.y),
+                                    1.0,
+                               )) // 2. scale to normalized device coordinate space
+                             * Transform::translate(Vector3f::new(
+                                    -screen_window.min.x,
+                                    -screen_window.max.y,
+                                    0.0,
+                               )); // 1. move upper-left corner of the screen to the origin
         let raster_to_screen = screen_to_raster.inverse();
 
         let raster_to_camera = camera_to_screen.inverse() * raster_to_screen;
@@ -39,9 +50,8 @@ impl PerspectiveCamera {
         return PerspectiveCamera {
             camera_to_world: camera_to_world,
 
-            camera_to_screen: camera_to_screen,
             raster_to_camera: raster_to_camera,
-
+            camera_to_screen: camera_to_screen,
             screen_to_raster: screen_to_raster,
             raster_to_screen: raster_to_screen,
         };
