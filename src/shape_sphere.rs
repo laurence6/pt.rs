5473ab3c7cfe13d0d8514ffba52f4f6bc4013759
Laurence <liuxy6@gmail.com>
2017-06-13 12:36:13 +0800

    Update Sphere

--- src/shape_sphere.rs
+++ src/shape_sphere.rs
@@ -1,32 +1,29 @@
 use std::rc::Rc;
 
 use bbox::BBox3f;
-use common::Float;
+use common::{Float, EPSILON};
 use interaction::Interaction;
 use material::Material;
 use ray::Ray;
 use shape::Shape;
-use vector::Point3f;
+use vector::{Normal3f, Point3f};
 
 pub struct Sphere {
     center: Point3f,
     radius: Float,
-
-    bbox: BBox3f,
 }
 
 impl Sphere {
     pub fn new(center: Point3f, radius: Float) -> Sphere {
-        let min = Point3f::new(center.x - radius, center.y - radius, center.z - radius);
-        let max = Point3f::new(center.x + radius, center.y + radius, center.z + radius);
-        let bbox = BBox3f { min: min, max: max };
-        return Sphere { center: center, radius: radius, bbox: bbox };
+        Sphere { center: center, radius: radius }
     }
 }
 
 impl Shape for Sphere {
     fn bbox(&self) -> BBox3f {
-        return self.bbox;
+        let min = Point3f::new(self.center.x - self.radius, self.center.y - self.radius, self.center.z - self.radius);
+        let max = Point3f::new(self.center.x + self.radius, self.center.y + self.radius, self.center.z + self.radius);
+        return BBox3f { min: min, max: max };
     }
 
     fn intersect_p(&self, ray: &Ray) -> bool {
@@ -34,10 +31,101 @@ impl Shape for Sphere {
     }
 
     fn intersect(&self, ray: &Ray) -> Option<Interaction> {
-        unimplemented!()
+        let to = ray.origin - self.center;
+        let b = ray.direction.dot(to);
+        let c = to.dot(to) - self.radius.powi(2);
+        let d = b.powi(2) - c;
+
+        if d < 0. {
+            return None;
+        }
+
+        let d = d.sqrt();
+
+        let t = -b - d;
+        if EPSILON < t && t < ray.t_max {
+            let p = ray.position(t);
+            let n = Normal3f::from((p - self.center).normalize());
+            return Some(Interaction {
+                p: p,
+                n: n,
+                ..Default::default()
+            });
+        }
+
+        let t = -b + d;
+        if EPSILON < t && t < ray.t_max {
+            let p = ray.position(t);
+            let n = Normal3f::from((self.center - p).normalize());
+            return Some(Interaction {
+                p: p,
+                n: n,
+                ..Default::default()
+            });
+        }
+
+        return None;
     }
 
     fn material(&self) -> Rc<Material> {
         unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use ray::Ray;
+    use shape::Shape;
+    use shape_sphere::Sphere;
+    use vector::{Vector3f, Normal3f, Point3f};
+
+    #[test]
+    fn test_intersect() {
+        let sphere = Sphere::new(Point3f::new(2., 2., 2.), 1.);
+
+        let ray = Ray {
+            origin: Point3f::new(2., 0., 2.),
+            direction: Vector3f::new(0., 1., 0.),
+            t_max: 10.,
+        };
+        let interaction = sphere.intersect(&ray).unwrap();
+        assert_eq!(interaction.p, Point3f::new(2., 1., 2.));
+        assert_eq!(interaction.n, Normal3f::new(0., -1., 0.));
+
+        let ray = Ray {
+            origin: Point3f::new(2., 4., 2.),
+            direction: Vector3f::new(0., -1., 0.),
+            t_max: 10.,
+        };
+        let interaction = sphere.intersect(&ray).unwrap();
+        assert_eq!(interaction.p, Point3f::new(2., 3., 2.));
+        assert_eq!(interaction.n, Normal3f::new(0., 1., 0.));
+
+        let ray = Ray {
+            origin: Point3f::new(2., 2., 2.),
+            direction: Vector3f::new(0., 1., 0.),
+            t_max: 10.,
+        };
+        let interaction = sphere.intersect(&ray).unwrap();
+        assert_eq!(interaction.p, Point3f::new(2., 3., 2.));
+        assert_eq!(interaction.n, Normal3f::new(0., -1., 0.));
+
+        let ray = Ray {
+            origin: Point3f::new(0., 0., 2.),
+            direction: Vector3f::new(7.109401, 6.9729223, 2.91366),
+            t_max: 10.,
+        };
+        let interaction = sphere.intersect(&ray).unwrap();
+
+        let ray = Ray {
+            origin: Point3f::new(2., 0., 2.),
+            direction: Vector3f::new(0., 1., 1.),
+            t_max: 10.,
+        };
+        let interaction = sphere.intersect(&ray);
+        if let None = interaction {
+            let interaction = interaction.unwrap();
+            panic!("interaction found: p:{:?} n:{:?}", interaction.p, interaction.n);
+        }
     }
 }
