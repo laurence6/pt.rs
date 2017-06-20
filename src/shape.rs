c8b34d59cd2fddddfd9a42bc5b84fadaabf1b3d6
Laurence <liuxy6@gmail.com>
2017-06-20 12:16:38 +0800

    Add util func intersect()

--- src/shape.rs
+++ src/shape.rs
@@ -10,7 +10,19 @@ pub trait Shape {
     fn bbox(&self) -> BBox3f;
     /// intersect_p returns whether or not the ray intersects this shape.
     fn intersect_p(&self, ray: &Ray) -> bool;
-    /// intersect determines whether the ray intersects this shape and if an intersection occurs, returns the details of the intersection and t value of the ray at the intersection point.
+    /// intersect determines whether the ray intersects this shape.
+    /// If an intersection occurs, it returns the detail of the intersection (Interaction.shape is NOT initialized in this method) and t value of the ray at the intersection point.
     fn intersect(&self, ray: &Ray) -> Option<(Interaction, Float)>;
     fn material(&self) -> Rc<Material>;
+}
+
+/// intersect determines whether the ray intersects this shape.
+/// If an intersection occurs, the detail of the intersection (Interaction.shape IS initialized in this method) is returned and Ray.t_max is updated with the t value at the intersection point.
+pub fn intersect(shape: &Rc<Shape>, ray: &mut Ray) -> Option<Interaction> {
+    if let Some((mut i, t)) = shape.intersect(ray) {
+        ray.t_max = t;
+        i.shape = Some(shape.clone());
+        return Some(i);
+    }
+    return None;
 }
