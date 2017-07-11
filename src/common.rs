c46beec1f64be143a2b56ee9ab42830a25cad5ba
Laurence <liuxy6@gmail.com>
2017-07-11 22:03:57 +0800

    Update common
      - Add MACHINE_EPSILON
      - Add next_float_down() next_float_up() quadratic()

--- src/common.rs
+++ src/common.rs
@@ -1,15 +1,18 @@
 use std::f32;
+use std::mem::{swap, transmute};
 
-pub const FLOAT_MAX: f32     = f32::MAX;
+pub const FLOAT_MAX:     f32 = f32::MAX;
 pub const FLOAT_MIN_POS: f32 = f32::MIN_POSITIVE;
-pub const EPSILON: f32       = f32::EPSILON * 0.5;
-pub const INFINITY: f32      = f32::INFINITY;
+pub const EPSILON:       f32 = f32::EPSILON;
+pub const INFINITY:      f32 = f32::INFINITY;
+
+pub const MACHINE_EPSILON: f32 = EPSILON * 0.5;
 /// Max number less than 1.
 pub const ONE_MINUS_EPSILON: f32 = 1. - EPSILON;
 
 pub const PI: f32 = f32::consts::PI;
 
-pub fn clamp<T: PartialOrd>(x: T, low: T, high: T) -> T {
+pub fn clamp<T>(x: T, low: T, high: T) -> T where T: PartialOrd {
     debug_assert!(low <= high);
     if x < low {
         return low;
@@ -25,5 +28,62 @@ pub fn lerp(t: f32, v1: f32, v2: f32) -> f32 {
 }
 
 pub fn gamma(x: f32) -> f32 {
-    (x * EPSILON) / (1. - x * EPSILON)
+    (x * MACHINE_EPSILON) / (1. - x * MACHINE_EPSILON)
+}
+
+pub fn next_float_down(mut x: f32) -> f32 {
+    if x.is_infinite() && x < 0. {
+        return x;
+    }
+    if x == 0. {
+        x = -0.;
+    }
+
+    let mut bits = unsafe { transmute::<f32, u32>(x) };
+    if x <= 0. {
+        bits += 1;
+    } else {
+        bits -= 1;
+    }
+
+    return unsafe { transmute::<u32, f32>(bits) };
+}
+
+pub fn next_float_up(mut x: f32) -> f32 {
+    if x.is_infinite() && x > 0. {
+        return x;
+    }
+    if x == -0. {
+        x = 0.;
+    }
+
+    let mut bits = unsafe { transmute::<f32, u32>(x) };
+    if x >= 0. {
+        bits += 1;
+    } else {
+        bits -= 1;
+    }
+
+    return unsafe { transmute::<u32, f32>(bits) };
+}
+
+pub fn quadratic(a: f64, b: f64, c: f64) -> Option<(f64, f64)> {
+    let discrim = b * b - 4. * a * c;
+    if discrim < 0. {
+        return None;
+    }
+
+    let root_discrim = discrim.sqrt();
+    let q = if b < 0. {
+        -0.5 * (b - root_discrim)
+    } else {
+        -0.5 * (b + root_discrim)
+    };
+
+    let (mut t0, mut t1) = (q / a, c / q);
+    if t0 > t1 {
+        swap(&mut t0, &mut t1);
+    }
+
+    return Some((t0, t1));
 }
