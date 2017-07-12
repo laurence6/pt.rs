a37ae6a22543af388b19a86fbc9d4abcabd831c9
Laurence <liuxy6@gmail.com>
2017-07-12 14:39:59 +0800

    Update Interaction & Sphere
      - Add p_err, dpdu, dpdv fields for Interaction
      - Impl more robust spawn_ray
      - Impl more robust ray-sphere intersection computation
      - Impl partial derivative computation for Sphere

--- src/interaction.rs
+++ src/interaction.rs
@@ -1,6 +1,6 @@
 use std::rc::Rc;
 
-use common::{INFINITY, ONE_MINUS_EPSILON};
+use common::{INFINITY, ONE_MINUS_EPSILON, next_float_down, next_float_up};
 use ray::Ray;
 use shape::Shape;
 use vector::{Vector3f, Normal3f, Point3f};
@@ -8,25 +8,49 @@ use vector::{Vector3f, Normal3f, Point3f};
 #[derive(Default, Clone)]
 pub struct Interaction {
     pub p: Point3f,
+    pub p_err: Vector3f,
+
     pub n: Normal3f,
+    pub dpdu: Vector3f,
+    pub dpdv: Vector3f,
+
     pub wo: Vector3f,
     pub shape: Option<Rc<Shape>>,
 }
 
 impl Interaction {
+    fn offset_ray_origin(&self, v: Vector3f) -> Point3f {
+        let n = Vector3f::from(self.n);
+        let d = n.abs().dot(self.p_err);
+        let mut offset = n * d;
+        if n.dot(v) < 0. {
+            offset *= -1.;
+        }
+        let mut p = self.p + offset;
+        for i in 0..3 {
+            if offset[i] > 0. {
+                p[i] = next_float_up(p[i]);
+            } else if offset[i] < 0. {
+                p[i] = next_float_down(p[i]);
+            }
+        }
+        return p;
+    }
+
     pub fn spawn_ray(&self, direction: Vector3f) -> Ray {
         Ray {
-            origin: self.p,
+            origin: self.offset_ray_origin(direction),
             direction,
             t_max: INFINITY,
         }
     }
 
     pub fn spawn_ray_to(&self, i: Point3f) -> Ray {
-        Ray {
-            origin: self.p,
-            direction: i - self.p,
+        let direction = i - self.p;
+        return Ray {
+            origin: self.offset_ray_origin(direction),
+            direction,
             t_max: ONE_MINUS_EPSILON,
-        }
+        };
     }
 }
--- src/shape_sphere.rs
+++ src/shape_sphere.rs
@@ -1,12 +1,12 @@
 use std::rc::Rc;
 
 use bbox::BBox3f;
-use common::EPSILON;
+use common::{PI, gamma, quadratic};
 use interaction::Interaction;
 use material::Material;
 use ray::Ray;
 use shape::Shape;
-use vector::{Normal3f, Point3f};
+use vector::{Vector3f, Normal3f, Point3f};
 
 pub struct Sphere {
     center: Point3f,
@@ -16,6 +16,58 @@ pub struct Sphere {
 impl Sphere {
     pub fn new(center: Point3f, radius: f32) -> Sphere {
         Sphere { center, radius }
+    }
+
+    fn world_to_local(&self, p: Point3f) -> Point3f {
+        p + -self.center
+    }
+
+    fn local_to_world(&self, p: Point3f) -> Point3f {
+        p + self.center
+    }
+
+    // return dpdu, dpdv
+    fn compute_partial_derivative(&self, Point3f { x, y, z }: Point3f) -> (Vector3f, Vector3f) {
+        let dpdu = Vector3f::new(
+            -2. * PI * y,
+            2. * PI * x,
+            0.,
+        );
+        let r_sin_theta = (x * x + y * y).sqrt(); // r * sin(theta) = (r^2 - z^2)^0.5 or (x^2 + y^2)^0.5
+        let dpdv = Vector3f::new(
+            PI * z * x/r_sin_theta,
+            PI * z * y/r_sin_theta,
+            PI * -r_sin_theta,
+        );
+        return (dpdu, dpdv);
+    }
+
+    fn compute_interaction(&self, ray: &Ray, t: f32) -> Interaction {
+        let mut p = ray.position(t);
+        p *= self.radius / Vector3f::from(p).length();
+
+        let p_err = Vector3f::from(p).abs() * gamma(5.);
+
+        let n = {
+            let n = Normal3f::from(Vector3f::from(p).normalize());
+            if Vector3f::from(ray.origin).length() < self.radius {
+                -n
+            } else {
+                n
+            }
+        };
+
+        let (dpdu, dpdv) = self.compute_partial_derivative(p);
+
+        return Interaction {
+            p: self.local_to_world(p),
+            p_err,
+            n,
+            dpdu,
+            dpdv,
+            wo: -ray.direction,
+            shape: None,
+        };
     }
 }
 
@@ -31,35 +83,30 @@ impl Shape for Sphere {
     }
 
     fn intersect(&self, ray: &Ray) -> Option<(Interaction, f32)> {
-        let to = ray.origin - self.center;
-        let b = ray.direction.dot(to);
-        let c = to.dot(to) - self.radius.powi(2);
-        let d = b.powi(2) - c;
+        let mut ray = ray.clone();
+        ray.origin = self.world_to_local(ray.origin);
 
-        if d < 0. {
+        let o = ray.origin;
+        let d = ray.direction;
+        let (ox, oy, oz) = (o.x as f64, o.y as f64, o.z as f64);
+        let (dx, dy, dz) = (d.x as f64, d.y as f64, d.z as f64);
+        let r = self.radius as f64;
+        let a = dx * dx + dy * dy + dz * dz;
+        let b = 2. * (dx * ox + dy * oy + dz * oz);
+        let c = ox * ox + oy * oy + oz * oz - r * r;
+
+        let s = quadratic(a, b, c);
+        if s.is_none() {
             return None;
         }
 
-        let d = d.sqrt();
-
-        let t = -b - d;
-        if EPSILON < t && t < ray.t_max {
-            let p = ray.position(t);
-            let n = Normal3f::from((p - self.center).normalize());
-            return Some((
-                Interaction { p, n, ..Default::default() },
-                t,
-            ));
+        let (t0, t1) = s.unwrap();
+        let (t0, t1) = (t0 as f32, t1 as f32);
+        if 0. < t0 && t0 < ray.t_max {
+            return Some((self.compute_interaction(&ray, t0), t0));
         }
-
-        let t = -b + d;
-        if EPSILON < t && t < ray.t_max {
-            let p = ray.position(t);
-            let n = Normal3f::from((self.center - p).normalize());
-            return Some((
-                Interaction { p, n, ..Default::default() },
-                t,
-            ));
+        if 0. < t1 && t1 < ray.t_max {
+            return Some((self.compute_interaction(&ray, t1), t1));
         }
 
         return None;
