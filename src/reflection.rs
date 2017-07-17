2c31e8c7c20410b6491f55a3177994417aaf9694
Laurence <liuxy6@gmail.com>
2017-07-17 17:03:07 +0800

    Add BSDF

--- src/reflection.rs
+++ src/reflection.rs
@@ -1,3 +1,6 @@
+use std::rc::Rc;
+
+use interaction::Interaction;
 use sampling::cosine_sample_hemisphere;
 use spectrum::Spectrum;
 use vector::{Vector3f, Point2f};
@@ -31,5 +34,158 @@ pub trait BxDF {
         let flag = self.flag();
         debug_assert!(flag <= ALL);
         return (t & flag) == t;
+    }
+}
+
+const MAX_BXDF: usize = 4;
+
+pub struct BSDF {
+    n_bxdf: usize,
+    bxdfs: [Option<Rc<BxDF>>; MAX_BXDF],
+    eta: f32,
+    n: Vector3f,
+    s: Vector3f,
+    t: Vector3f,
+}
+
+impl BSDF {
+    /// Relative index of refraction.
+    pub fn new(eta: f32, i: &Interaction) -> BSDF {
+        let n = Vector3f::from(i.n);
+        let s = i.dpdu.normalize();
+        let t = n.cross(s);
+        return BSDF {
+            n_bxdf: 0,
+            bxdfs: Default::default(),
+            eta,
+            n,
+            s,
+            t,
+        };
+    }
+
+    /// Add a BxDF component.
+    pub fn add(&mut self, bxdf: Rc<BxDF>) {
+        self.bxdfs[self.n_bxdf] = Some(bxdf);
+        self.n_bxdf += 1;
+    }
+
+    fn world_to_local(&self, v: Vector3f) -> Vector3f {
+        Vector3f::new(
+            v.dot(self.s),
+            v.dot(self.t),
+            v.dot(self.n),
+        )
+    }
+
+    fn local_to_world(&self, v: Vector3f) -> Vector3f {
+        Vector3f::new(
+            self.s.x * v.x + self.t.x * v.y + self.n.x * v.z,
+            self.s.y * v.x + self.t.y * v.y + self.n.y * v.z,
+            self.s.z * v.x + self.t.z * v.y + self.n.z * v.z,
+        )
+    }
+
+    pub fn f(&self, wo_w: Vector3f, wi_w: Vector3f) -> Spectrum {
+        let wo = self.world_to_local(wo_w);
+        if wo.z == 0. {
+            return Default::default();
+        }
+        let wi = self.world_to_local(wi_w);
+
+        let reflect = wi_w.dot(self.n) * wo_w.dot(self.n) > 0.;
+
+        let mut f = Spectrum::default();
+        for i in 0..self.n_bxdf {
+            let bxdf = self.bxdfs[i].clone().unwrap();
+            if (reflect && bxdf.has_flag(REFLECTION))
+                || (!reflect && bxdf.has_flag(TRANSMISSION)) {
+                f += bxdf.f(wo, wi);
+            }
+        }
+
+        return f;
+    }
+
+    pub fn sample_f(&self, wo_w: Vector3f, sample: Point2f) -> (Vector3f, Spectrum, BxDFFlag) {
+        let n_bxdf_f = sample[0] * self.n_bxdf as f32;
+        let n_bxdf = n_bxdf_f.floor() as usize;
+        let bxdf = self.bxdfs[n_bxdf].clone().unwrap();
+
+        // remap sample to [0, 1)
+        let sample = Point2f::new(n_bxdf_f - n_bxdf as f32, sample[1]);
+
+        let wo = self.world_to_local(wo_w);
+        if wo.z == 0. {
+            return Default::default();
+        }
+        let (wi, mut f) = bxdf.sample_f(wo, sample);
+        let wi_w = self.local_to_world(wi);
+
+        if !bxdf.has_flag(SPECULAR) {
+            let reflect = wi_w.dot(self.n) * wo_w.dot(self.n) > 0.;
+            f = Spectrum::default();
+            for i in 0..self.n_bxdf {
+                let bxdf = self.bxdfs[i].clone().unwrap();
+                if (reflect && bxdf.has_flag(REFLECTION)) || (!reflect && bxdf.has_flag(TRANSMISSION)) {
+                    f += bxdf.f(wo, wi);
+                }
+            }
+        }
+
+        return (wi_w, f, bxdf.flag());
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use common::EPSILON;
+    use interaction::Interaction;
+    use reflection::{BxDF, BSDF, BxDFFlag, REFLECTION, TRANSMISSION, DIFFUSE, GLOSSY, SPECULAR};
+    use spectrum::Spectrum;
+    use vector::{Vector3f, Normal3f, Point3f};
+
+    #[test]
+    fn test_has_type() {
+        struct TestBxDF {}
+        impl BxDF for TestBxDF {
+            fn flag(&self) -> BxDFFlag {
+                REFLECTION | DIFFUSE
+            }
+
+            fn f(&self, wo: Vector3f, wi: Vector3f) -> Spectrum {
+                unimplemented!()
+            }
+        }
+
+        let bxdf = TestBxDF{};
+        assert!(bxdf.has_flag(REFLECTION));
+        assert!(bxdf.has_flag(DIFFUSE));
+        assert_eq!(bxdf.has_flag(TRANSMISSION), false);
+        assert_eq!(bxdf.has_flag(GLOSSY), false);
+        assert_eq!(bxdf.has_flag(SPECULAR), false);
+    }
+
+    #[test]
+    fn test_bsdf_world_loacl() {
+        let bsdf = BSDF::new(
+            1.,
+            &Interaction {
+                p: Point3f::new(1., 1., 1.),
+                n: Normal3f::new(-1., 0., 0.),
+                dpdu: Vector3f::new(0., -1., 0.),
+                ..Default::default()
+            },
+        );
+
+        let v = Vector3f::new(1., 2., 3.);
+
+        let v1 = bsdf.local_to_world(v);
+        assert_eq!(v1, Vector3f::new(-3., -1., 2.));
+
+        let v2 = bsdf.world_to_local(v1);
+        for i in 0..3 {
+            assert!((v2[i] - v[i]).abs() < EPSILON);
+        }
     }
 }
