928758923dfaf837b9e5658f3b7be2fad94782c8
Laurence <liuxy6@gmail.com>
2017-07-18 16:43:01 +0800

    Add LambertianReflectionMaterial

---
+++ src/material_lambertian_reflection.rs
@@ -0,0 +1,28 @@
+use std::rc::Rc;
+
+use bxdf_lambertian_reflection::LambertianReflectionBRDF;
+use interaction::Interaction;
+use material::Material;
+use reflection::BSDF;
+use texture::Texture;
+
+pub struct LambertianReflectionMaterial {
+    kd: Rc<Texture>,
+}
+
+impl LambertianReflectionMaterial {
+    pub fn new(kd: Rc<Texture>) -> LambertianReflectionMaterial {
+        LambertianReflectionMaterial { kd }
+    }
+}
+
+impl Material for LambertianReflectionMaterial {
+    fn compute_scattering(&self, i: &Interaction) -> BSDF {
+        let r = self.kd.evaluate(i);
+        let mut bsdf = BSDF::new(1., i);
+        if !r.is_black() {
+            bsdf.add(Rc::new(LambertianReflectionBRDF::new(r)));
+        }
+        return bsdf;
+    }
+}
