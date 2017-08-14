71281b31398fbed67d11d382a9e5e7f1bfcb680c
Laurence <liuxy6@gmail.com>
2017-08-14 16:27:16 +0800

    Add FresnelNoOp

--- src/fresnel.rs
+++ src/fresnel.rs
@@ -3,3 +3,17 @@ use spectrum::Spectrum;
 pub trait Fresnel {
     fn evaluate(&self, cos_i: f32) -> Spectrum;
 }
+
+pub struct FresnelNoOp {}
+
+impl FresnelNoOp {
+    pub fn new() -> FresnelNoOp {
+        FresnelNoOp {}
+    }
+}
+
+impl Fresnel for FresnelNoOp {
+    fn evaluate(&self, cos_i: f32) -> Spectrum {
+        Spectrum::new(1., 1., 1.)
+    }
+}
