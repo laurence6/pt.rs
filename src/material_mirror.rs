bf0f4f44903b8c1ec5f816e047a85a46d85ea454
Laurence <liuxy6@gmail.com>
2017-08-15 12:12:05 +0800

    Add MirrorMaterial

---
+++ src/material_mirror.rs
@@ -0,0 +1,30 @@
+use std::rc::Rc;
+
+use bxdf_specular_reflection::SpecularReflectionBRDF;
+use common::INFINITY;
+use fresnel::FresnelNoOp;
+use interaction::Interaction;
+use material::Material;
+use reflection::BSDF;
+use texture::Texture;
+
+pub struct MirrorMaterial {
+    kr: Rc<Texture>,
+}
+
+impl MirrorMaterial {
+    pub fn new(kr: Rc<Texture>) -> MirrorMaterial {
+        MirrorMaterial { kr }
+    }
+}
+
+impl Material for MirrorMaterial {
+    fn compute_scattering(&self, i: &Interaction) -> BSDF {
+        let mut bsdf = BSDF::new(1., i);
+        let r = self.kr.evaluate(i).clamp(0., INFINITY);
+        if !r.is_black() {
+            bsdf.add(Box::new(SpecularReflectionBRDF::new(r, Box::new(FresnelNoOp::new()))));
+        }
+        return bsdf;
+    }
+}
