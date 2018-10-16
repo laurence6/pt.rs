c0ef1e53c19d6158143cbb4b9643ff96672440d0
Laurence <liuxy6@gmail.com>
2018-10-16 19:09:38 -0400

    Update MetalMaterial
      - Create SpecularReflectionBRDF if roughness == 0
      - Clamp r

--- src/material_metal.rs
+++ src/material_metal.rs
@@ -1,6 +1,8 @@
 use std::sync::Arc;
 
 use bxdf_microfacet_reflection::MicrofacetReflectionBRDF;
+use bxdf_specular_reflection::SpecularReflectionBRDF;
+use common::INFINITY;
 use fresnel::FresnelConductor;
 use interaction::Interaction;
 use material::Material;
@@ -27,17 +29,20 @@ impl Material for MetalMaterial {
     fn compute_scattering(&self, i: &Interaction) -> BSDF {
         let mut bsdf = BSDF::new(1., i);
 
-        let r = self.r.evaluate(i);
-
-        let rough_u = self.roughness_u.evaluate(i);
-        let rough_v = self.roughness_v.evaluate(i);
-        let distrib = MicrofacetDistribution::new(rough_u, rough_v);
-
+        let r = self.r.evaluate(i).clamp(0., INFINITY);
         let eta = self.eta.evaluate(i);
         let k = self.k.evaluate(i);
+        let rough_u = self.roughness_u.evaluate(i);
+        let rough_v = self.roughness_v.evaluate(i);
+
         let fresnel = FresnelConductor::new(Spectrum::from(1.), eta, k);
 
-        bsdf.add(MicrofacetReflectionBRDF::new(r, distrib, fresnel));
+        if rough_u == 0. && rough_v == 0. {
+            bsdf.add(SpecularReflectionBRDF::new(r, fresnel));
+        } else {
+            let distrib = MicrofacetDistribution::new(rough_u, rough_v);
+            bsdf.add(MicrofacetReflectionBRDF::new(r, distrib, fresnel));
+        }
 
         return bsdf;
     }
